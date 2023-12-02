c6cb3e6c4d16ee15d82de419d929f26d7907f600
moelshenawy <mohamedgk5@gmail.com>
2023-12-02 05:29:45 +0200

    ..

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,29 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.scss";
+import { createTheme } from "@mui/material";
+import { ThemeProvider } from "styled-components";
+// import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const breakpoints = {
+  values: {
+    xs: 0,
+    sm: 600,
+    md: 900,
+    lg: 1200,
+    xl: 1536,
+  },
+};
+
+const theme = createTheme({
+  backgroundColor: "red",
+});
+
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />;
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />;
+      </Layout>
+    </ThemeProvider>
   );
 }
