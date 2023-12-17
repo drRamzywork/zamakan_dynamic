e6892b48fcf7875a8791b31160fe5378dbf298a5
moelshenawy <mohamedgk5@gmail.com>
2023-12-17 21:07:12 +0200

    ..

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,31 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.scss";
 
-export default function App({ Component, pageProps }) {
+// export default function App({ Component, pageProps }) {
+//   return (
+//     <Layout>
+//       <Component {...pageProps} />
+//     </Layout>
+//   );
+// }
+
+import App from "next/app";
+
+function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   );
 }
+
+MyApp.getInitialProps = async (appContext) => {
+  // Call the page's `getInitialProps` and fill `pageProps`
+  const pageProps = await App.getInitialProps(appContext);
+
+  // Perform additional operations if needed
+
+  return { pageProps };
+};
+
+export default MyApp;
